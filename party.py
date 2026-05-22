fca0c3adac595f3549b57d42804bb00afc3eb432
pol <masterlydata509@gmail.com>
2026-05-22 01:27:39 -0600

    Added constants.py and stage.png started progress on party scene drawing

--- party.py
+++ party.py
@@ -1,5 +1,9 @@
-# import pygame
+import pygame
 import random
+
+import constants
+
+# SCREEN_SIZE = (1280, 720)
 
 dinosaur_characteristics = [
     "Tall", "Fast", "Flying", "Strong", "Herbivore", "Horns", "Water", "Armoured" 
@@ -7,39 +11,32 @@ dinosaur_characteristics = [
 
 class Kid:
     def __init__(self, favourite_ranking, is_host):
-        characteristic1 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        characteristic2 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        while (characteristic1 == characteristic2):
-            characteristic2 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        self.favourite_characteristics = [characteristic1, characteristic2]
+        self.favourite_characteristics = random.sample(dinosaur_characteristics, 2)
             
         self.favourite_ranking = favourite_ranking 
         self.is_host = is_host
-        pass
 
 class Dino:
     def __init__(self):
         self.base_earn = random.randint(2,9)
         
-        characteristic1 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        characteristic2 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        while (characteristic1 == characteristic2):
-            characteristic2 = dinosaur_characteristics[random.randint(0,len(dinosaur_characteristics)-1)]
-        self.characteristics = [characteristic1, characteristic2]
-        
-        pass
+        self.characteristics = random.sample(dinosaur_characteristics, 2)
 
 class Party:
-    def __init__(self, kid_count):
+    stage_path = "assets/stage.png"
+    
+    def __init__(self, kid_count, dino_count):
         self.party_kids = [Kid(1, True)]
         for i in range(kid_count - 1):
-            self.party_kids.append(Kid(i + 1, False))
+            self.party_kids.append(Kid(i + 2, False))
         self.party_dinos:list[Dino] = []
 
-        for i in range(4):
+        for i in range(dino_count):
             self.party_dinos.append(Dino())
+            
+        self.stage = pygame.transform.scale(pygame.image.load(self.stage_path).convert_alpha(), constants.SCREEN_SIZE)
 
-    def calculate_win(self):
+    def calculate_earn(self):
         party_earn = 0
         for kid in self.party_kids:
             personal_earn = 5
@@ -54,16 +51,28 @@ class Party:
 
                 personal_earn += dino_earn
             
-            personal_earn *= (kid.favourite_ranking / len(self.party_kids)) + 0.8
+            personal_earn *= ((len(self.party_kids) - kid.favourite_ranking) / len(self.party_kids)) + 0.8
             party_earn += personal_earn
 
         return party_earn
 
+    def draw_party(self, surface):
+        self.draw_stage(surface)
+        pass
+        
+    def draw_stage(self, surface):
+        surface.blit(self.stage, self.stage.get_rect(topleft=(0,0)))
+        pass
+        
+    def draw_audience(self):
+        pass
 
 
-party_size = 5
-party = Party(party_size)
+
+#
+# party_size = random.randint(3,8)
+# party = Party(party_size, 3)
 # for i in range(party_size):
 #     print(party.party_kids[i].favourite_characteristics, party.party_kids[i].favourite_ranking, party.party_kids[i].is_host)
-
-print(party.calculate_win())
+#
+# print(party.calculate_earn())
