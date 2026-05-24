bf223fe4e916a56174b71ec364d87d14fb6c19d5
pol <masterlydata509@gmail.com>
2026-05-24 16:52:28 -0600

    Merge completed

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ screen = pygame.display.set_mode(constants.SCREEN_SIZE, pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
 
+
 class DisplayScreen:
     def __init__(self, size):
         self.surface = pygame.Surface(size)
-        self.og_rect = pygame.Rect((0,0), size)
-        self.display_rect = pygame.Rect((0,0), size)
-    
+        self.og_rect = pygame.Rect((0, 0), size)
+        self.display_rect = pygame.Rect((0, 0), size)
+
     def update_size(self, new_size):
-        self.display_rect = self.og_rect.fit(pygame.Rect((0,0), new_size))
+        self.display_rect = self.og_rect.fit(pygame.Rect((0, 0), new_size))
 
     def render(self, screen: pygame.Surface):
-        screen.blit(pygame.transform.scale(self.surface, self.display_rect.size), self.display_rect)
+        screen.blit(
+            pygame.transform.scale(self.surface, self.display_rect.size),
+            self.display_rect,
+        )
+
 
 dscreen = DisplayScreen(constants.SCREEN_SIZE)
 game_state = "title_screen"
@@ -35,7 +40,7 @@ mont_bold_30 = pygame.freetype.Font("assets/Mont-HeavyDEMO.otf", 30)
 mont_15 = pygame.freetype.Font("assets/Mont-ExtraLightDEMO.otf", 15)
 mont_bold_15 = pygame.freetype.Font("assets/Mont-HeavyDEMO.otf", 15)
 
-#current run data
+# current run data
 dinos = []
 money = 1000
 reputation = 100
@@ -43,21 +48,26 @@ current_day = 1
 party_contracts = []
 
 
-
-#game state change
+# game state change
 EVENT_GAME_STATE_CHANGE = pygame.event.custom_type()
+
 
 class BasicSprite:
     def __init__(self, image, size, pos, is_centered=True):
-        self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), size)
+        self.image = pygame.transform.scale(
+            pygame.image.load(image).convert_alpha(), size
+        )
         if is_centered:
             self.rect = self.image.get_rect(center=pos)
         else:
             self.rect = self.image.get_rect(topleft=pos)
+
     def tick(self):
         pass
+
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
 
 class BasicText:
     def __init__(self, font: pygame.freetype.Font, pos, text, color="black"):
@@ -65,24 +75,38 @@ class BasicText:
         self.pos = pos
         self.text = text
         self.color = color
+
     def tick(self):
         pass
+
     def draw(self, surface):
         self.font.render_to(surface, self.pos, self.text, fgcolor=self.color)
 
-#general dino structure
+
+# general dino structure
 class DinoCharacter:
     def __init__(self, name, image, size, tier, traits, recruit_cost, duration):
         self.name = name
-        self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), (size, size))
+        self.image = pygame.transform.scale(
+            pygame.image.load(image).convert_alpha(), (size, size)
+        )
         self.tier = tier
         self.traits = traits
         self.recruit_cost = recruit_cost
         self.inital_duration = duration
         self.days_remaining = duration
 
+
 class DinoInfoBox:
-    def __init__(self, dino_character: DinoCharacter, rect_pos, rect_size, font: pygame.freetype.Font, bg_color, text_color):
+    def __init__(
+        self,
+        dino_character: DinoCharacter,
+        rect_pos,
+        rect_size,
+        font: pygame.freetype.Font,
+        bg_color,
+        text_color,
+    ):
         self.rect = pygame.rect.Rect((0, 0), rect_size)
         self.rect.center = rect_pos
         self.image = pygame.Surface(self.rect.size).convert_alpha()
@@ -91,10 +115,10 @@ class DinoInfoBox:
         self.text_color = text_color
         self.dino_c = dino_character
         self.visible = False
-    
+
     def set_dino_character(self, dino_character: DinoCharacter):
         self.dino_c = dino_character
-    
+
     def tick(self):
         pass
 
@@ -106,26 +130,53 @@ class DinoInfoBox:
         y = self.rect.y + 10
         spacing = 40
 
-        self.font.render_to(surface, (x, y+0*spacing), f"Name: {self.dino_c.name}", self.text_color)
-        self.font.render_to(surface, (x, y+1*spacing), f"Tier: {self.dino_c.tier}", self.text_color)
-        self.font.render_to(surface, (x, y+2*spacing), f"Recruit Cost: {self.dino_c.recruit_cost}", self.text_color)
-        self.font.render_to(surface, (x, y+3*spacing), f"Contract Duration: {self.dino_c.days_remaining}", self.text_color)
-        self.font.render_to(surface, (x, y+4*spacing), f"Traits:  ", self.text_color)
+        self.font.render_to(
+            surface, (x, y + 0 * spacing), f"Name: {self.dino_c.name}", self.text_color
+        )
+        self.font.render_to(
+            surface, (x, y + 1 * spacing), f"Tier: {self.dino_c.tier}", self.text_color
+        )
+        self.font.render_to(
+            surface,
+            (x, y + 2 * spacing),
+            f"Recruit Cost: {self.dino_c.recruit_cost}",
+            self.text_color,
+        )
+        self.font.render_to(
+            surface,
+            (x, y + 3 * spacing),
+            f"Contract Duration: {self.dino_c.days_remaining}",
+            self.text_color,
+        )
+        self.font.render_to(
+            surface, (x, y + 4 * spacing), f"Traits:  ", self.text_color
+        )
         ix = x
         for i in self.dino_c.traits:
             ix += self.font.get_rect(None).right
-            if ix >= self.rect.x + self.rect.w/2:
+            if ix >= self.rect.x + self.rect.w / 2:
                 ix = x
                 y += spacing
-            #iy = y + 80 + self.font.get_rect(None).y
-            self.font.render_to(surface, (ix, y+4*spacing), f"{i} ", self.text_color)
+            # iy = y + 80 + self.font.get_rect(None).y
+            self.font.render_to(
+                surface, (ix, y + 4 * spacing), f"{i} ", self.text_color
+            )
             ix += 10
 
-example_dino = DinoCharacter("Placeholder", "assets/dino_placeholder1.png", 200, "Real", ["Dumb", "Stupid", "Jurassic"], 50, 3)
 
-#shop code
+example_dino = DinoCharacter(
+    "Placeholder",
+    "assets/dino_placeholder1.png",
+    200,
+    "Real",
+    ["Dumb", "Stupid", "Jurassic"],
+    50,
+    3,
+)
+
+
+# shop code
 class ShopDino:
-
     def __init__(self, dino_character: DinoCharacter, pos):
         self.image = pygame.transform.scale(dino_character.image, (100, 100))
         self.dino_character = dino_character
@@ -135,7 +186,7 @@ class ShopDino:
         self.stop_roam = 0
         self.start_roam = 0
         self.direction = [0, 0]
-    
+
     def grab(self):
         self.state = "grabbed"
 
@@ -156,13 +207,13 @@ class ShopDino:
         if self.state == "idle":
             if current_tick >= self.next_roam:
                 self.state = "roaming"
-                self.stop_roam = current_tick + 250*random.randint(1,4)
+                self.stop_roam = current_tick + 250 * random.randint(1, 4)
                 self.start_roam = current_tick
                 self.direction = [random.randint(-1, 1), random.randint(-1, 1)]
-        
+
         if self.state == "grabbed":
             self.rect.center = mouse_pos
-        
+
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > 1280:
@@ -173,32 +224,57 @@ class ShopDino:
             self.rect.bottom = 720
 
     def draw(self, surface):
-        mont_bold_15.render_to(surface, (self.rect.left + 20, self.rect.bottom + 10), f"Cost: {self.dino_character.recruit_cost}", "white")
-        mont_bold_15.render_to(surface, (self.rect.left + 10, self.rect.bottom + 25), f"For: {self.dino_character.inital_duration} day(s)", "white")
+        mont_bold_15.render_to(
+            surface,
+            (self.rect.left + 20, self.rect.bottom + 10),
+            f"Cost: {self.dino_character.recruit_cost}",
+            "white",
+        )
+        mont_bold_15.render_to(
+            surface,
+            (self.rect.left + 10, self.rect.bottom + 25),
+            f"For: {self.dino_character.inital_duration} day(s)",
+            "white",
+        )
         if self.state == "idle" or self.dino_character.tier == "Cutout":
             surface.blit(self.image, self.rect)
         elif self.state == "roaming":
             tick_progress = pygame.time.get_ticks() - self.start_roam
             total = self.stop_roam - self.start_roam
-            surface.blit(self.image, (self.rect.x, self.rect.y + 5*math.sin((tick_progress/total)*math.pi*6)))
+            surface.blit(
+                self.image,
+                (
+                    self.rect.x,
+                    self.rect.y + 5 * math.sin((tick_progress / total) * math.pi * 6),
+                ),
+            )
         elif self.state == "grabbed":
             surface.blit(pygame.transform.rotate(self.image, 45), self.rect)
 
-#admin phase code
+
+# admin phase code
 class DinoInfoBlock:
     def __init__(self, pos, dino_character: DinoCharacter):
         self.rect = pygame.Rect(pos, (200, 100))
         self.dino_image = dino_character.image
         self.dino_character = dino_character
-        self.name_text = BasicText(mont_15, (self.rect.x, self.rect.y), str(dino_character.name))
-        self.tier_text = BasicText(mont_15, (self.rect.x, self.rect.y+20), str(dino_character.tier))
+        self.name_text = BasicText(
+            mont_15, (self.rect.x, self.rect.y), str(dino_character.name)
+        )
+        self.tier_text = BasicText(
+            mont_15, (self.rect.x, self.rect.y + 20), str(dino_character.tier)
+        )
         traits_text = ""
         for i in dino_character.traits:
             traits_text += f"{i}  "
-        self.traits_text = BasicText(mont_15, (self.rect.x, self.rect.y+40), str(traits_text))
-        self.days_text = BasicText(mont_15, (self.rect.x, self.rect.y+60), str(dino_character.days_remaining))
+        self.traits_text = BasicText(
+            mont_15, (self.rect.x, self.rect.y + 40), str(traits_text)
+        )
+        self.days_text = BasicText(
+            mont_15, (self.rect.x, self.rect.y + 60), str(dino_character.days_remaining)
+        )
         self.selected = False
-    
+
     def tick(self):
         pass
 
@@ -210,15 +286,16 @@ class DinoInfoBlock:
         self.traits_text.draw(surface)
         self.days_text.draw(surface)
 
+
 class DinoList:
     def __init__(self, dino_character_list):
         self.dino_list = []
         for i in range(len(dino_character_list)):
-            self.dino_list.append(DinoInfoBlock((0, i*100), dino_character_list[i]))
-    
+            self.dino_list.append(DinoInfoBlock((0, i * 100), dino_character_list[i]))
+
     def tick(self):
         pass
-    
+
     def draw(self, surface: pygame.Surface):
         for i in self.dino_list:
             i.draw(surface)
@@ -233,17 +310,21 @@ def adjust_pos_to_display(pos):
     adjusted_y = adjusted_y * ratio_y
     return int(adjusted_x), int(adjusted_y)
 
-#shop things
+
+# shop things
 next_button = BasicSprite("assets/end_shop_button.png", (150, 120), (10, 550), False)
 buy_basket = BasicSprite("assets/basket.png", (200, 200), (1100, 400))
 money_count_text = BasicText(mont_bold_30, (10, 10), "0", "white")
 shop_dinos = []
-dino_info_box = DinoInfoBox(None, (640, 360), (450, 300), mont_bold_30, (0,0,0,125), "white")
+dino_info_box = DinoInfoBox(
+    None, (640, 360), (450, 300), mont_bold_30, (0, 0, 0, 125), "white"
+)
 shop_floor = BasicSprite("assets/stone_floor.png", (1280, 720), (0, 0), False)
 
-#admin phase things
+# admin phase things
 dino_list = DinoList([])
 gray_backdrop = BasicSprite("assets/backdrop.png", (1280, 720), (0, 0), False)
+
 
 def generate_dinos(amount, day, reputation):
     cutouts = 0
@@ -257,9 +338,9 @@ def generate_dinos(amount, day, reputation):
     weak_traits = constants.dinosaur_characteristics
 
     for i in range(amount):
-        for i in range(day * int(reputation/50)):
+        for i in range(day * int(reputation / 50)):
             quality += random.randint(3, 6)
-        if random.randint(1,10) == 10:
+        if random.randint(1, 10) == 10:
             reals += 1
         elif quality <= 70:
             cutouts += 1
@@ -272,88 +353,94 @@ def generate_dinos(amount, day, reputation):
         name = "Placeholder"
         image = "assets/dino_placeholder1.png"
         size = 150
-        cost = random.randint(1,3) * 5
-        duration = random.randint(4,8)
+        cost = random.randint(1, 3) * 5
+        duration = random.randint(4, 8)
         traits = []
-        for i in range(random.randint(3,5)):
+        for i in range(random.randint(3, 5)):
             if random.randint(1, 5) == 5:
                 traits.append(random.choice(strong_traits))
                 luck = random.randint(0, 3)
                 if luck == 0:
-                    cost = int(cost*1.5)
+                    cost = int(cost * 1.5)
                 elif luck == 1:
                     duration -= 1
             else:
                 traits.append(random.choice(weak_traits))
                 luck = random.randint(0, 2)
                 if luck == 0:
-                    cost = int(cost*0.8)
+                    cost = int(cost * 0.8)
                 elif luck == 1:
                     duration += 1
         if duration <= 0:
             duration = 1
-        generated_dinos.append(DinoCharacter(name, image, size, "Cutout", traits, cost, duration))
-    
+        generated_dinos.append(
+            DinoCharacter(name, image, size, "Cutout", traits, cost, duration)
+        )
+
     for i in range(costumes):
         name = "Placeholder"
         image = "assets/dino_placeholder1.png"
         size = 175
-        cost = random.randint(3,6) * 10
-        duration = random.randint(4,6)
+        cost = random.randint(3, 6) * 10
+        duration = random.randint(4, 6)
         traits = []
-        for i in range(random.randint(3,5)):
+        for i in range(random.randint(3, 5)):
             if random.randint(1, 3) == 3:
                 traits.append(random.choice(strong_traits))
                 luck = random.randint(0, 2)
                 if luck == 0:
-                    cost = int(cost*1.25)
+                    cost = int(cost * 1.25)
                 elif luck == 1:
                     duration -= 1
             else:
                 traits.append(random.choice(weak_traits))
                 luck = random.randint(0, 2)
                 if luck == 0:
-                    cost = int(cost*0.7)
+                    cost = int(cost * 0.7)
                 elif luck == 1:
                     duration += 1
         if duration <= 0:
             duration = 1
-        generated_dinos.append(DinoCharacter(name, image, size, "Costume", traits, cost, duration))
-    
+        generated_dinos.append(
+            DinoCharacter(name, image, size, "Costume", traits, cost, duration)
+        )
+
     for i in range(reals):
         name = "Placeholder"
         image = "assets/dino_placeholder1.png"
         size = 200
-        cost = random.randint(7,15) * 10
-        duration = random.randint(1,3)
+        cost = random.randint(7, 15) * 10
+        duration = random.randint(1, 3)
         traits = []
-        for i in range(random.randint(4,6)):
+        for i in range(random.randint(4, 6)):
             if random.randint(1, 5) >= 3:
                 traits.append(random.choice(strong_traits))
                 luck = random.randint(0, 5)
                 if luck == 0:
-                    cost = int(cost*1.2)
+                    cost = int(cost * 1.2)
                 elif luck == 1:
                     duration -= 1
             else:
                 traits.append(random.choice(weak_traits))
                 luck = random.randint(0, 1)
                 if luck == 0:
-                    cost = int(cost*0.8)
+                    cost = int(cost * 0.8)
                 elif luck == 1:
                     duration += 1
         if duration <= 0:
             duration = 1
-        generated_dinos.append(DinoCharacter(name, image, size, "Real", traits, cost, duration))
-    
+        generated_dinos.append(
+            DinoCharacter(name, image, size, "Real", traits, cost, duration)
+        )
+
     return generated_dinos
-                
 
 
 objects = []
 
 mouse_just_pressed = False
 mouse_down = False
+
 
 while running:
     mouse_pos = adjust_pos_to_display(pygame.mouse.get_pos())
@@ -377,7 +464,9 @@ while running:
                 objects.append(shop_floor)
                 objects.append(buy_basket)
                 for i in generate_dinos(random.randint(5, 10), current_day, reputation):
-                    shop_dinos.append(ShopDino(i, (random.randint(50, 900), random.randint(100, 620))))
+                    shop_dinos.append(
+                        ShopDino(i, (random.randint(50, 900), random.randint(100, 620)))
+                    )
                 for i in shop_dinos:
                     objects.append(i)
                 objects.append(next_button)
@@ -389,10 +478,22 @@ while running:
                 objects.append(gray_backdrop)
                 objects.append(dino_list)
                 party_contracts = [party.Party(100, 0, 3), party.Party(100, 0, 5)]
-                objects.append(party.PartyContractsList(party_contracts, (640, 30), True))
-    
+                objects.append(
+                    party.PartyContractsList(party_contracts, (640, 30), True)
+                )
+
+            elif event.new_state == "phase_results":
+                test_party1 = party.Party(100, 0, 3)
+                test_party2 = party.Party(100, 0, 5)
+                test_party3 = party.Party(100, 0, 7)
+
+                results = party.Results([test_party1, test_party2, test_party3])
+                objects.append(results)
+
     if game_state == "title_screen":
-        pygame.event.post(pygame.event.Event(EVENT_GAME_STATE_CHANGE, new_state="phase_shop"))
+        pygame.event.post(
+            pygame.event.Event(EVENT_GAME_STATE_CHANGE, new_state="phase_shop")
+        )
 
     elif game_state == "phase_shop":
         money_count_text.text = f"Money: {str(money)}"
@@ -402,7 +503,10 @@ while running:
                 i.grab()
                 break
             elif not mouse_down and i.state == "grabbed":
-                if buy_basket.rect.collidepoint(mouse_pos) and i.dino_character.recruit_cost <= money:
+                if (
+                    buy_basket.rect.collidepoint(mouse_pos)
+                    and i.dino_character.recruit_cost <= money
+                ):
                     money -= i.dino_character.recruit_cost
                     dinos.append(i.dino_character)
                     shop_dinos.remove(i)
@@ -418,7 +522,9 @@ while running:
 
             i.tick(mouse_pos)
         if mouse_just_pressed and next_button.rect.collidepoint(mouse_pos):
-            pygame.event.post(pygame.event.Event(EVENT_GAME_STATE_CHANGE, new_state="phase_admin"))
+            pygame.event.post(
+                pygame.event.Event(EVENT_GAME_STATE_CHANGE, new_state="phase_admin")
+            )
 
     elif game_state == "phase_contracts":
         pass
@@ -431,7 +537,7 @@ while running:
 
     screen.fill("black")
     dscreen.surface.fill("white")
-    
+
     for i in objects:
         i.draw(dscreen.surface)
 
@@ -439,5 +545,5 @@ while running:
     pygame.display.flip()
 
     clock.tick(60)
-    
+
 pygame.quit()
--- party.py
+++ party.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.freetype
+import math
 import random
 
 
@@ -7,68 +8,88 @@ import constants
 
 # SCREEN_SIZE = (1280, 720)
 
+
 class BasicText:
     def __init__(self, font: pygame.freetype.Font, pos, text, color="black"):
         self.font = font
         self.pos = pos
         self.text = text
         self.color = color
+
     def tick(self):
         pass
+
     def draw(self, surface):
         self.font.render_to(surface, self.pos, self.text, fgcolor=self.color)
 
+
 class Kid:
     def __init__(self, favourite_ranking, is_host):
-        self.favourite_characteristics = random.sample(constants.dinosaur_characteristics, 2)
-            
-        self.favourite_ranking = favourite_ranking 
+        self.favourite_characteristics = random.sample(
+            constants.dinosaur_characteristics, 2
+        )
+
+        self.favourite_ranking = favourite_ranking
         self.is_host = is_host
-        self.audience_image = pygame.transform.scale(pygame.image.load("assets/kid1.png").convert_alpha(), (180,180))
-        
+        self.audience_image = pygame.transform.scale(
+            pygame.image.load("assets/kid1.png").convert_alpha(), (180, 180)
+        )
+
     def draw(self, surface, x, y):
-        surface.blit(self.audience_image, self.audience_image.get_rect(topleft=(x,y)))
+        surface.blit(self.audience_image, self.audience_image.get_rect(topleft=(x, y)))
+
 
 class Dino:
     def __init__(self):
-        self.base_earn = random.randint(2,9)
-        
+        self.base_earn = random.randint(2, 9)
+
         self.characteristics = random.sample(constants.dinosaur_characteristics, 2)
+
 
 class Party:
     stage_path = "assets/stage.png"
     audience_path = "assets/audience.png"
-    
+
     def __init__(self, price, budget: int, kid_count):
         self.price = price
         self.budget = budget
+
         self.party_kids = [Kid(1, True)]
         for i in range(kid_count - 1):
             self.party_kids.append(Kid(i + 2, False))
-        self.party_dinos:list[Dino] = [None, None, None]
-            
-        self.stage_image = pygame.transform.scale(pygame.image.load(self.stage_path).convert_alpha(), constants.SCREEN_SIZE)
-        self.audience_image = pygame.transform.scale(pygame.image.load(self.audience_path).convert_alpha(), (1280/1.5,720/1.5))
+        self.party_dinos: list[Dino] = [Dino(), Dino(), Dino()]
+        self.base_pay = 0
+        self.host_name = "Henry"
+
+        self.stage_image = pygame.transform.scale(
+            pygame.image.load(self.stage_path).convert_alpha(), constants.SCREEN_SIZE
+        )
+        self.audience_image = pygame.transform.scale(
+            pygame.image.load(self.audience_path).convert_alpha(),
+            (1280 / 1.5, 720 / 1.5),
+        )
 
     def set_dino(self, index, dino_character):
         self.party_dinos[index] = dino_character
 
-    def calculate_earn(self):
+    def calculate_tip(self):
         party_earn = 0
         for kid in self.party_kids:
             personal_earn = 5
-            
+
             for dino in self.party_dinos:
                 dino_earn = dino.base_earn
-                
+
                 for characteristic in dino.characteristics:
                     for kid_fav_char in kid.favourite_characteristics:
-                        if (characteristic == kid_fav_char):
+                        if characteristic == kid_fav_char:
                             dino_earn *= 2
 
                 personal_earn += dino_earn
-            
-            personal_earn *= ((len(self.party_kids) - kid.favourite_ranking) / len(self.party_kids)) + 0.8
+
+            personal_earn *= (
+                (len(self.party_kids) - kid.favourite_ranking) / len(self.party_kids)
+            ) + 0.8
             party_earn += personal_earn
 
         return party_earn
@@ -76,12 +97,15 @@ class Party:
     def draw(self, surface):
         self.draw_stage(surface)
         self.draw_audience(surface)
-        
+
     def draw_stage(self, surface):
-        surface.blit(self.stage_image, self.stage_image.get_rect(topleft=(0,0)))
-        
+        surface.blit(self.stage_image, self.stage_image.get_rect(topleft=(0, 0)))
+
     def draw_audience(self, surface):
-        surface.blit(self.audience_image, self.audience_image.get_rect(topleft=(90,420)))
+        surface.blit(
+            self.audience_image, self.audience_image.get_rect(topleft=(90, 420))
+        )
+
 
 class PartyContractsList:
     def __init__(self, parties, pos, is_owned=False):
@@ -98,21 +122,22 @@ class PartyContractsList:
 
     def tick(self):
         pass
-    
+
     def draw(self, surface: pygame.Surface):
         for i in self.party_box_list:
             i.draw(surface, self.pos[0], self.pos[1])
+
 
 class PartyBox:
     def __init__(self, party: Party, index, size):
         self.party = party
         self.index = index
         self.size = size
-    
+
     def draw(self, surface, x, starting_y):
         y = starting_y + 210 * self.index
-        
-        #rect = pygame.Rect(50, y, 1180, 200)
+
+        # rect = pygame.Rect(50, y, 1180, 200)
         pygame.draw.rect(surface, pygame.Color(31, 221, 255), ((x, y), self.size))
         self.draw_mugshot(surface, x + 30, starting_y + 10)
         self.draw_stats(surface, x + 230, starting_y + 10)
@@ -141,9 +166,13 @@ class PartyBox:
             budget == "Endless"
         econ_stat = BasicText(sysfont_10, (x, y), f"Budget: {budget}")
         y += 20
-        kid_count = BasicText(sysfont_10, (x, y), f"Number of Guests: {len(self.party.party_kids)}")
+        kid_count = BasicText(
+            sysfont_10, (x, y), f"Number of Guests: {len(self.party.party_kids)}"
+        )
         y += 20
-        minimum_pay = BasicText(sysfont_10, (x, y), f"Pay (before tip): {self.party.price}")
+        minimum_pay = BasicText(
+            sysfont_10, (x, y), f"Pay (before tip): {self.party.price}"
+        )
         y += 20
 
         host_favs = ""
@@ -160,16 +189,62 @@ class PartyBox:
         for i in guest_favs_list:
             guest_favs += f"{i}  "
 
-        host_fav_characteristics = BasicText(sysfont_10, (x, y), f"Henry likes dinos that are: {host_favs}")
+        host_fav_characteristics = BasicText(
+            sysfont_10, (x, y), f"Henry likes dinos that are: {host_favs}"
+        )
         y += 20
-        guest_fav_characteristics = BasicText(sysfont_10, (x, y), f"The guests like dinos that are: {guest_favs}")
-        
+        guest_fav_characteristics = BasicText(
+            sysfont_10, (x, y), f"The guests like dinos that are: {guest_favs}"
+        )
+
         name.draw(surface)
         econ_stat.draw(surface)
         kid_count.draw(surface)
         minimum_pay.draw(surface)
         host_fav_characteristics.draw(surface)
         guest_fav_characteristics.draw(surface)
+
+
+class Results:
+    def __init__(self, parties: list[Party]):
+        self.parties = parties
+
+    def draw(self, surface: pygame.surface.Surface):
+
+        for i, party in enumerate(self.parties):
+            self.draw_mugshots(surface, i, party)
+            self.draw_stats(surface, i, party)
+
+    def draw_mugshots(self, surface: pygame.surface.Surface, i, party: Party):
+        x, y = 70, 30 + 210 * i
+        for kid in party.party_kids:
+            if kid.is_host:
+                kid.draw(surface, x, y)
+                return
+        pass
+
+    def draw_stats(self, surface: pygame.surface.Surface, i, party: Party):
+        font_10 = pygame.freetype.SysFont("arial", 10)
+        font_20 = pygame.freetype.SysFont("arial", 20)
+
+        tip = math.trunc(party.calculate_tip())
+
+        y = 30 + 210 * i
+        name_text = BasicText(font_20, (270, y), f"{party.host_name}'s party")
+        y += 60
+        base_pay_text = BasicText(font_10, (270, y), f"Base pay: {party.base_pay}")
+        y += 20
+        tip_pay_text = BasicText(font_10, (270, y), f"Tip pay: {tip}")
+        y += 20
+        total_pay_text = BasicText(
+            font_10, (270, y), f"Total pay: {tip + party.base_pay}"
+        )
+
+        name_text.draw(surface)
+        base_pay_text.draw(surface)
+        total_pay_text.draw(surface)
+        tip_pay_text.draw(surface)
+
 
 #
 # party_size = random.randint(3,8)
